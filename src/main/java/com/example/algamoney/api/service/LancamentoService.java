10c7c779a1db57ae3fb0af721e08e106342d055e
HELIO.FRANCA@RSINET.COM.BR <heliordf@hotmail.com>
2021-05-20 00:04:26 -0300

    first commit

---
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,34 @@
+package com.example.algamoney.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@Service
+public class LancamentoService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	public Lancamento salvar(Lancamento lancamento) {
+		boolean existePessoa = pessoaRepository.existsById(lancamento.getPessoa().getCodigo());
+		Pessoa pessoa=null;;
+		if (existePessoa) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo()).get();
+			System.out.println("-----------------------> Pessoa: " + pessoa);
+		}
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		return lancamentoRepository.save(lancamento);
+	}
+
+}
